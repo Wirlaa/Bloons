823b93cd0c9a8d9f8bea8f328bffba3aa7f22cc2
Filip Piskorski <fpfpfp2002@gmail.com>
2023-02-19 16:07:07 +0100

    Added icon

--- src/main/java/agh/gui/App.java
+++ src/main/java/agh/gui/App.java
@@ -5,8 +5,10 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
 import java.util.Objects;
 
 
@@ -19,6 +21,7 @@ public class App extends Application {
         stage.initStyle(StageStyle.TRANSPARENT);
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/menu.fxml")));
         Scene scene = new Scene(root);
+        stage.getIcons().add(new Image("/32x32icon.png"));
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/menu.css")).toExternalForm());
         stage.setScene(scene);
         stage.show();
